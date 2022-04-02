2411397a97c9c4632a4d71d78fb2f0c9c189bd2b
Marina Albalà <70697960+Vizalt@users.noreply.github.com>
2022-04-02 23:38:40 +0200

    add anim

--- RPG-Code/Code/Source/Modules/ModulePlayer.cpp
+++ RPG-Code/Code/Source/Modules/ModulePlayer.cpp
@@ -6,4 +6,33 @@
 
 ModulePlayer::ModulePlayer(App* application, bool start_enabled) : Module(application, start_enabled)
 {
+	name.Create("player");
+
+	walkAnimDown.PushBack({ 3,1,27,31 });
+	walkAnimDown.PushBack({ 36,1,27,31 });
+	walkAnimDown.PushBack({ 68,1,27,31 });
+	walkAnimDown.loop = true;
+	walkAnimDown.speed = 0.1f;
+
+	walkAnimUp.PushBack({ 3,97,27,31 });
+	walkAnimUp.PushBack({ 35,1,27,31 });
+	walkAnimUp.PushBack({ 67,1,27,31 });
+	walkAnimUp.loop = true;
+	walkAnimUp.speed = 0.1f;
+
+	walkAnimL.PushBack({ 3,33,27,31 });
+	walkAnimL.PushBack({ 36,32,27,32 });
+	walkAnimL.PushBack({ 68,33,27,32 });
+	walkAnimL.loop = true;
+	walkAnimL.speed = 0.1f;
+
+	walkAnimR.PushBack({ 3,65,27,32 });
+	walkAnimR.PushBack({ 36,64,27,32 });
+	walkAnimR.PushBack({ 67,65,27,32 });
+	walkAnimR.loop = true;
+	walkAnimR.speed = 0.1f;
 }
+
+// Destructor
+ModulePlayer::~ModulePlayer()
+{}
--- RPG-Code/Code/Source/Modules/ModulePlayer.h
+++ RPG-Code/Code/Source/Modules/ModulePlayer.h
@@ -1,9 +1,51 @@
 #ifndef __MODULEPLAYER_H__
 #define __MODULEPLAYER_H__
 
+#include "Module.h"
+#include "Animation.h"
+
+struct SDL_Texture;
+struct SDL_Surface;
+struct Collider;
+
 class ModulePlayer :public Module {
 public:
 	ModulePlayer(App* application, bool start_enabled = true);
 
+	~ModulePlayer();
+
+	//// Called before render is available
+	//bool Awake(pugi::xml_node&);
+
+	//// Called before the first frame
+	//bool Start();
+
+	//// Called before all Updates
+	//bool PreUpdate();
+
+	//// Called each loop iteration
+	//bool Update(float dt);
+
+	//// Called before all Updates
+	//bool PostUpdate();
+
+	//// Called before quitting
+	//bool CleanUp();
+
+	// Load / Save
+	//bool LoadState(pugi::xml_node&);
+	//bool SaveState(pugi::xml_node&) const;
+
+private:
+	//cargar textura del jugador
+	SDL_Texture* texture = nullptr;
+
+	//player animations
+	Animation* currentAnimation = nullptr;
+	Animation 
+		walkAnimUp,
+		walkAnimDown,
+		walkAnimR,
+		walkAnimL;
 };
 #endif
