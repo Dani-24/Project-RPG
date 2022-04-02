38728640a130ec38ce4a4cd26ccbf2f2a2db154f
Marina Albalà <70697960+Vizalt@users.noreply.github.com>
2022-04-02 23:19:32 +0200

    add module player

---
+++ RPG-Code/Code/Source/Modules/ModulePlayer.cpp
@@ -0,0 +1,9 @@
+#include "App.h"
+#include "Render.h"
+#include "Textures.h"
+#include "Input.h"
+#include "ModulePlayer.h"
+
+ModulePlayer::ModulePlayer(App* application, bool start_enabled) : Module(application, start_enabled)
+{
+}
