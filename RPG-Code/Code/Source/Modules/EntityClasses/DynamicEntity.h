380b7d5b5cbd31fd58d660ce4738248cf725135e
David <79161120+Divangus@users.noreply.github.com>
2022-04-08 21:17:02 +0200

    class Stats

--- RPG-Code/Code/Source/Modules/EntityClasses/DynamicEntity.h
+++ RPG-Code/Code/Source/Modules/EntityClasses/DynamicEntity.h
@@ -20,6 +20,41 @@ enum class DynamicType
 	INTERACTABLE
 };
 
+class Stats
+{
+public:
+	int health;
+	int maxHealth;
+
+	int level;
+	int attack;
+	int deffense;
+	int mana;
+
+	Stats() {
+		this->level = 0;
+
+		this->maxHealth = 100;
+		this->health = 100;
+
+		this->attack = 1;
+		this->deffense = 1;
+		this->mana = 10;
+	}
+
+	Stats(int level, int maxHealth, int attack, int deffense, int mana) {
+		this->level = level;
+
+		this->maxHealth = maxHealth;
+		this->health = maxHealth;
+
+		this->attack = attack;
+		this->deffense = deffense;
+		this->mana = mana;
+	}
+
+};
+
 class DynamicEntity : public Entity
 {
 public:
@@ -47,6 +82,8 @@ public:
 	int dynamicThing;
 
 	Collider* baseCollider;
+
+	Stats EStats;
 
 };
 
