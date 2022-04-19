d1a22cf11995f88b222908a14028631a5e2a9bcc
laia <laiapachon02@gmail.com>
2022-04-19 21:01:39 +0200

    initial dialog traduction

--- RPG-Code/Code/Source/Modules/Scenes/Gameloop/Stages.cpp
+++ RPG-Code/Code/Source/Modules/Scenes/Gameloop/Stages.cpp
@@ -148,42 +148,42 @@ bool Stages::PostUpdate()
 			switch (epilogFase)
 			{
 			case 0:
-				app->font->DrawTextDelayed("Hola, alma viajera", epilogX + 35, epilogY);
+				app->font->DrawTextDelayed("Hello, adventurer soul", epilogX + 35, epilogY);
 				break;
 			case 1:
-				app->font->DrawTextDelayed("Canonicamente, un camion te acaba de atropellar", epilogX - 100, epilogY);
+				app->font->DrawTextDelayed("Canonically, a truck have just run over you", epilogX - 100, epilogY);
 				break;
 			case 2:
-				app->font->DrawTextDelayed("Camion-kun, quien goberna en este mundo", epilogX - 70, epilogY);
+				app->font->DrawTextDelayed("Camion-kun, who rules this world", epilogX - 70, epilogY);
 				break;
 			case 3:
-				app->font->DrawTextDelayed("atropella cuerpos y trae sus almas aqui sin descanso", epilogX - 110, epilogY);
+				app->font->DrawTextDelayed("he runs over people and bring their souls here without breaks", epilogX - 110, epilogY);
 				break;
 			case 4:
-				app->font->DrawTextDelayed("con el objetivo de combatir en la torre", epilogX - 50, epilogY + 25, {255,0,0});
+				app->font->DrawTextDelayed("with the goal of fihting on the tower", epilogX - 50, epilogY + 25, {255,0,0});
 				break;
 			case 5:
-				app->font->DrawTextDelayed("Aunque bueno, yo venia a preguntarte cosas", epilogX -70, epilogY);
+				app->font->DrawTextDelayed("But well, I came to ask you some things", epilogX -70, epilogY);
 				break;
 			case 6:
-				app->font->DrawTextDelayed("y asumir tu genero con ello", epilogX, epilogY);
+				app->font->DrawTextDelayed("and asume your gender with them", epilogX, epilogY);
 				break;
 			case 7:
-				app->font->DrawTextDelayed("Bueno, como diria Oak, eres chico o chica?", epilogX -70, epilogY);
+				app->font->DrawTextDelayed("Wll, as Oak says, you are a boy or a girl?", epilogX -70, epilogY);
 				break;
 			case 8:
-				app->font->DrawTextDelayed("Elige con 1 o 2 y confirma con Espacio", epilogX -65, epilogY);
+				app->font->DrawTextDelayed("Choose with 1 or 2 andd confirm with Space", epilogX -65, epilogY);
 				break;
 			case 9:
 				if (playerPtr->PlayerErection == true) {
-					app->font->DrawTextDelayed("Con que eres un chico (Puedes seguir cambiando con 1 y 2)", epilogX - 150, epilogY);
+					app->font->DrawTextDelayed("OH! So you're a boy (You can still change your gender with 1 or 2)", epilogX - 150, epilogY);
 				}
 				else {
-					app->font->DrawTextDelayed("Con que eres una chica (Puedes seguir cambiando con 1 y 2)", epilogX - 150, epilogY);
+					app->font->DrawTextDelayed("OH!So you're a girl (You can still change your gender with 1 or 2", epilogX - 150, epilogY);
 				}
 				break;
 			case 10:
-				app->font->DrawTextDelayed("Una gran aventura te aguarda joven", epilogX - 50, epilogY);
+				app->font->DrawTextDelayed("An amazing adventure is waiting for you", epilogX - 50, epilogY);
 				break;
 			case 11:
 				ChangeStage(StageIndex::TOWN);
