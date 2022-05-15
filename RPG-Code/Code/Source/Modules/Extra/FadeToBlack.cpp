5175026df8552c5862d4872ef500b869f81d21be
laia <laiapachon02@gmail.com>
2022-05-15 16:15:15 +0200

    transitions

--- RPG-Code/Code/Source/Modules/Extra/FadeToBlack.cpp
+++ RPG-Code/Code/Source/Modules/Extra/FadeToBlack.cpp
@@ -31,6 +31,7 @@ bool FadeToBlack::Start()
 
 bool FadeToBlack::Update(float dt)
 {
+	step++;
 	if (currentStep == Fade_Step::NONE)
 	{
 		return true;
@@ -82,7 +83,66 @@ bool FadeToBlack::PostUpdate()
 		app->font->DrawText("Loading . . .", -app->camera->GetPos().x / app->win->GetScale() + app->win->GetWidth() / 2 - 110,
 			-app->camera->GetPos().y / app->win->GetScale() + app->win->GetHeight() / 2 - 25);
 	}
+	switch (transitionType) {
+	case  Fade_Step::TO_BLACK:
+		DoFadeToBlack();
+		break;
+	case  Fade_Step::FROM_BLACK:
+		DoFadeToBlack();
+		break;
+	case  Fade_Step::SQUARED:
+		Squared();
+		break;
 
+	case  Fade_Step::CIRCLE:
+		Circle();
+		break;
+
+	case  Fade_Step::SLASH:
+		Slash();
+		break;
+
+	case  Fade_Step::THEATRE:
+		Theatre();
+		break;
+
+	case  Fade_Step::DISSOLVE:
+		Dissolve();
+		break;
+
+	case  Fade_Step::ZOOM:
+		Zoom();
+		break;
+
+		//TODO 4: Executa la funció de la transisió
+	case  Fade_Step::CUT:
+		Cut();
+		break;
+		//
+
+	}
+
+	//CHANGING SCENE SWITCH
+	switch (transitionType) {
+
+		//TODO 5: Assigna el tipus de transisió perquè faci el canvi de mòdul al principi.
+	case  Fade_Step::CUT:
+		//
+
+	case  Fade_Step::ZOOM:
+	case Fade_Step::DISSOLVE:
+		if (step >= 1)
+			SceneChange();
+		break;
+
+	default:
+		if (2 * step >= transitionTime)
+			SceneChange();
+		break;
+	}
+
+	if (step >= transitionTime)
+		Disable();
 	return true;
 }
 
@@ -149,3 +209,135 @@ bool FadeToBlack::DoFadeToBlack(StageIndex stage, float frames)
 	}
 	return ret;
 }
+void Fade::SceneChange()
+{
+	preScene->Disable();
+	postScene->Enable();
+}
+
+
+
+void ModuleTransitions::Squared()
+{
+	int percentage = ((float)step / (float)transitionTime) * 40.0f;
+	int w = SCREEN_WIDTH / 10;
+	int h = SCREEN_HEIGHT / 10;
+
+	if (step * 2 >= transitionTime)
+		percentage = 40 - percentage;
+
+	for (int j = 0; j < percentage; ++j) {
+		for (int i = 0; i < percentage; ++i) {
+			App->renderer->DrawQuad(SDL_Rect{ w * i, h * (10 - j + i), w, h }, 0, 0, 0, 255);
+		}
+	}
+}
+
+void ModuleTransitions::Circle()
+{
+	if (sprite == nullptr)
+		sprite = App->textures->Load("Assets/circle.png");
+
+	float percentage = ((float)step / (float)transitionTime) * 2.0f;
+
+	if (step * 2 >= transitionTime)
+		percentage = 2.0f - percentage;
+
+	float scale = 5.0f * percentage;
+
+	App->renderer->DrawTexture(sprite, SCREEN_WIDTH / 2.0f - 150.0f * scale, SCREEN_HEIGHT / 2.0f - 150.0f * scale, scale);
+}
+
+void ModuleTransitions::Slash()
+{
+	if (sprite == nullptr)
+		sprite = App->textures->Load("Assets/slash.png");
+
+	float percentage = ((float)step / (float)transitionTime) * 200.0f;
+
+	if (step * 2 >= transitionTime)
+		percentage = 200 - percentage;
+
+	int scale = 3;
+
+	App->renderer->DrawTexture(sprite, ((SCREEN_WIDTH / 2) * ((100.0f - percentage) / 100.0f)) + ((SCREEN_WIDTH - (600 * scale)) / 2),
+		(SCREEN_HEIGHT - (300 * scale)) / 2, scale);
+
+	App->renderer->DrawTexture(sprite, ((SCREEN_WIDTH - (600 * scale)) / 2) - ((SCREEN_WIDTH / 2) * ((100.0f - percentage) / 100.0f)),
+		(SCREEN_HEIGHT - (300 * scale)) / 2, scale, true);
+}
+
+void ModuleTransitions::Theatre()
+{
+	if (sprite == nullptr)
+		sprite = App->textures->Load("Assets/semicircle.png");
+
+	float percentage = ((float)step / (float)transitionTime) * 360.0f;
+
+	int scale = 7;
+
+	App->renderer->DrawTexture(sprite, ((SCREEN_WIDTH - (300 * scale)) / 2), SCREEN_HEIGHT, scale, false, NULL, 1.0f, percentage, 150 * scale, 0);
+}
+
+void ModuleTransitions::Dissolve()
+{
+	//If screenshot is made, load it
+	if (App->renderer->screenshot) {
+		sprite = App->textures->Load("Assets/screenshot.bmp");
+	}
+
+	//Call for screenshot
+	if (sprite == nullptr) {
+		App->renderer->pendingToScreenshot = true;
+		return;
+	}
+
+	float percentage = ((float)step / (float)transitionTime) * 255.0f;
+
+	SDL_SetTextureBlendMode(sprite, SDL_BLENDMODE_BLEND);
+	SDL_SetTextureAlphaMod(sprite, 255.0f - percentage);
+
+	App->renderer->DrawTexture(sprite, 0, 0);
+}
+
+void ModuleTransitions::Zoom()
+{
+	//If screenshot is made, load it
+	if (App->renderer->screenshot) {
+		sprite = App->textures->Load("Assets/screenshot.bmp");
+	}
+
+	//Call for screenshot
+	if (sprite == nullptr) {
+		App->renderer->pendingToScreenshot = true;
+		return;
+	}
+
+	float percentage = ((float)step / (float)transitionTime) * 5.0f;
+
+	App->renderer->DrawTexture(sprite, 0, 0, 1.0f + percentage);
+
+}
+
+//TODO 6: Defineix la funció de transició
+void ModuleTransitions::Cut() {
+	//If screenshot is made, load it
+	if (App->renderer->screenshot) {
+		sprite = App->textures->Load("Assets/screenshot.bmp");
+	}
+
+	//Call for screenshot
+	if (sprite == nullptr) {
+		App->renderer->pendingToScreenshot = true;
+		return;
+	}
+
+	float percentage = ((float)step / (float)transitionTime) * ((float)SCREEN_HEIGHT / 2.0f);
+
+	SDL_Rect top = { 0, 0, SCREEN_WIDTH, SCREEN_HEIGHT / 2 };
+	SDL_Rect bot = { 0, SCREEN_HEIGHT / 2, SCREEN_WIDTH, SCREEN_HEIGHT / 2 };
+	App->renderer->DrawTexture(sprite, 0, -percentage, 1.0f, false, &top);
+	App->renderer->DrawTexture(sprite, 0, SCREEN_HEIGHT / 2 + percentage, 1.0f, false, &bot);
+}
+//
+
--- RPG-Code/Code/Source/Modules/Extra/FadeToBlack.h
+++ RPG-Code/Code/Source/Modules/Extra/FadeToBlack.h
@@ -8,9 +8,12 @@
 
 #include "Stages.h"
 
+
+
 class FadeToBlack : public Module
 {
 public:
+	
 	FadeToBlack(App* application, bool start_enabled = true);
 
 	~FadeToBlack();
@@ -29,16 +32,26 @@ public:
 
 	// Fade Between stages
 	bool DoFadeToBlack(StageIndex stage, float frames = 60);
-
+	
 private:
-
-	enum Fade_Step
-	{
+	enum class Fade_Step {
 		NONE,
 		TO_BLACK,
-		FROM_BLACK
-	}
-	currentStep = Fade_Step::NONE;
+		FROM_BLACK,
+		SQUARED,
+		CIRCLE,
+		SLASH,
+		THEATRE,
+		DISSOLVE,
+		ZOOM,
+
+		//TODO 1: Crea un nou tipus de transisió
+		CUT
+		//
+
+	} currentStep = Fade_Step::NONE;
+	
+	
 	//frame count
 	Uint32 frameCount = 0;
 	Uint32 maxFadeFrames = 0;
@@ -47,13 +60,51 @@ private:
 
 	Module* moduleToEnable = nullptr;
 	Module* moduleToDisable = nullptr;
+	int step;
+	int transitionTime;
+	Fade_Step transitionType;
+	Module* preScene;
+	Module* postScene;
 
 public:
 	bool fading;
+	void Transition(Module* preScene, Module* postScene, int transitionTime, Fade_Step transitionType)
+	{
+		//The module is already transitioning
+		if (transitionTime < 0) {
+			LOG("ERROR: INVALID TRANSITION TIME");
+			return;
+		}
+
+		//The module is already transitioning
+		if (isEnabled()) {
+			LOG("ERROR: ALREADY TRANSITIONING");
+			return;
+		}
+
+		Enable();
+		this->preScene = preScene;
+		this->postScene = postScene;
+		this->transitionTime = transitionTime;
+		this->transitionType = transitionType;
+	}
 
 private:
+	void SceneChange();
+
+	//TransitionModules
+	void Squared();
+	void Circle();
+	void Slash();
+	void Theatre();
+	void Dissolve();
+	void Zoom();
+
+	//TODO 3: Declara la funció per transicionar.
+	void Cut();
 	bool changeStage;
 	StageIndex stageToChange;
+	
 };
 
 #endif //__FADETOBLACK_H__
